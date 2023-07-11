9f7fbeea70a502de8a660cbe45cdee00f3706271
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2023-07-11 19:14:52 -0300

    type any

---
+++ src/A0001-type-annotation/A0002-type-any.ts
@@ -0,0 +1,8 @@
+// Utilize any apenas em  último caso
+function showMessege(msg: any) {
+  return msg;
+}
+
+console.log(showMessege([1, 2, 3]));
+console.log(showMessege("Ola"));
+console.log(showMessege(5));

fbf7145da0783c844ecb5655e3a6cec2955dae1f
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2023-07-11 20:42:11 -0300

    type enum

---
+++ src/A0009-type-enum/A0009-type-enum.ts
@@ -0,0 +1,23 @@
+//
+enum Cores {
+  VERMELHOR = 10, // 0
+  AZUL = 100, // 1
+  AMARELO = 200, // 2
+}
+
+enum Cores {
+  ROXO = "ROXO",
+  VERDE = 201,
+  ROSA,
+}
+
+// console.log(Cores);
+// console.log(Cores.VERMELHOR);
+// console.log(Cores[10]);
+// console.log(Cores.ROXO);
+
+function escolhaACor(cor: Cores): void {
+  console.log(Cores[cor]);
+}
+
+escolhaACor(Cores.ROXO);

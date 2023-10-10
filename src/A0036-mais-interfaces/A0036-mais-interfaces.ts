c081e2db47ba099ea48f7228a9ce47a88fe1955d
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2023-10-10 19:18:44 -0300

    declaration merging

---
+++ src/A0036-mais-interfaces/A0036-mais-interfaces.ts
@@ -0,0 +1,24 @@
+// Declatarion merging
+interface Pessoa {
+  nome: string;
+}
+
+interface Pessoa {
+  readonly sobrenome: string;
+}
+
+interface Pessoa {
+  readonly enderecos: readonly string[];
+}
+
+interface Pessoa {
+  idade?: number;
+}
+const pessoa1: Pessoa = {
+  nome: "Luiz",
+  sobrenome: "Miranda",
+  enderecos: ["Av. Brasil"],
+  idade: 30,
+};
+
+console.log(pessoa1);
